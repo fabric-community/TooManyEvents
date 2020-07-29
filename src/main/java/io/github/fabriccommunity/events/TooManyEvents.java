21f409794c72d2701205c6b38c6e99f47a953660
valoeghese <valoeghesemods@gmail.com>
2020-07-29 23:24:51 +1200

    Join us, and you will never have to worry about code thiefs and lawsuits. Our secret stash of proguard docs should be enough to get you started

--- src/main/java/io/github/fabriccommunity/events/TooManyEvents.java
+++ src/main/java/io/github/fabriccommunity/events/TooManyEvents.java
@@ -1,14 +1,15 @@
 package io.github.fabriccommunity.events;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import net.fabricmc.api.ModInitializer;
 
 public class TooManyEvents implements ModInitializer {
+	public static final Logger LOGGER = LogManager.getLogger("TooManyEvents");
+
 	@Override
 	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
-
-		System.out.println("Hello Fabric world!");
+		LOGGER.info("Haha events go brrrr");
 	}
 }
---
+++ src/main/java/io/github/fabriccommunity/events/mixin/MixinWorldRenderer.java
@@ -0,0 +1,28 @@
+package io.github.fabriccommunity.events.mixin;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import io.github.fabriccommunity.events.WorldRenderCallback;
+import net.minecraft.client.render.Camera;
+import net.minecraft.client.render.GameRenderer;
+import net.minecraft.client.render.LightmapTextureManager;
+import net.minecraft.client.render.WorldRenderer;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.client.world.ClientWorld;
+import net.minecraft.util.math.Matrix4f;
+
+@Mixin(WorldRenderer.class)
+public class MixinWorldRenderer {
+	@Shadow
+	private ClientWorld world;
+
+	@Inject(at = @At("RETURN"), method = "render")
+	private void addRendering(MatrixStack matrices, float tickDelta, long limitTime, boolean renderBlockOutline, Camera camera,
+			GameRenderer gameRenderer, LightmapTextureManager lightmapTextureManager, Matrix4f projection, CallbackInfo info) {
+		WorldRenderCallback.EVENT.invoker().onWorldRender(this.world, matrices, tickDelta, limitTime, renderBlockOutline, gameRenderer, projection);
+	}
+}
