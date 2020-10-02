7f7b18b1b16f55320806e19cebafacf79a9f8343
valoeghese <valoeghesemods@gmail.com>
2020-10-02 16:23:01 +1300

    yes

--- src/test/java/io/github/fabriccommunity/events/test/WakeTest.java
+++ src/test/java/io/github/fabriccommunity/events/test/WakeTest.java
@@ -2,7 +2,6 @@ package io.github.fabriccommunity.events.test;
 
 import io.github.fabriccommunity.events.play.PlayerInteractionEvents;
 import net.fabricmc.api.ModInitializer;
-import net.minecraft.entity.damage.DamageSource;
 
 public class WakeTest implements ModInitializer {
 	public static final boolean ENABLED = true;
@@ -12,7 +11,7 @@ public class WakeTest implements ModInitializer {
 		if (ENABLED) {
 			PlayerInteractionEvents.WAKE_UP.register((player, updateSleepingPlayers, sleepT20) -> {
 				if (!player.world.isClient() && player.getHungerManager().getFoodLevel() < 16) {
-					player.damage(DamageSource.GENERIC, 1);
+					player.setHealth(player.getHealth() - 1);
 				}
 			});
 		}
